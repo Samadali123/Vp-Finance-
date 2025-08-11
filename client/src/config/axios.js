0ce3106f096f2dde00e4c4d69a725dbcd392cbde
= <=>
2025-08-11 18:24:01 +0530

    add chnage

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,3 +1,16 @@
+// import axios from "axios";
+
+// const API_URL = import.meta.env.VITE_API_URL;
+// const axiosInstance = axios.create({
+//   baseURL: API_URL,
+//   createDetails: true,
+// });
+
+// export default axiosInstance;
+
+
+
+
 import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
